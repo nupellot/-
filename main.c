ba04386556403e0052f4d3ec23dc22e266126bf6
Dmitriy Korotkov <54524404+nupellot@users.noreply.github.com>
2021-12-28 18:53:48 +0000

    Добавлен вывод файлов в консоль

--- main.c
+++ main.c
@@ -13,17 +13,22 @@ typedef struct {  // Структура студента.
 
 int main(void) {
 	// Открываем исходный файл
-  FILE* source = fopen("source", "r+");
+  FILE* source = fopen("source", "r");
 	if (source == NULL) {
 		printf("Ошибка открытия файла\n");
 		exit(1);
 	}
-	printf("Файл успешно открыт\n");
+	// Выводим содержимое исходного файла.
+	printf("\033[45m%s\033[0m\n", "Содержимое исходного файла до обработки:");
+	char c;
+	while ((c = fgetc(source)) != EOF && c != '#') printf("%c", c);
+	fseek(source, 0, 0); // Уходим обратно к началу файла.
+	// printf("Файл успешно открыт\n");
 
 	// Объявляем функцию для дальнейшего использования.
 	int getAmountOfFails(student*);
 
-	char c = fgetc(source);
+	c = fgetc(source);
 	// Считаем количество строк.
 	int amountOfStudents = 0;
 	do {
@@ -43,12 +48,12 @@ int main(void) {
 	fclose(source);
 	
 	// Печатаем информацию о студентах.
-	printf("В структуру записаны следующие студенты:\n");
-	for (int i = 0; i < amountOfStudents; i++) {
-		printf("%s %s %d %d %d %d\n", allStudents[i].name, allStudents[i].classLetter, allStudents[i].firstGrade, allStudents[i].secondGrade, allStudents[i].thirdGrade, allStudents[i].fourthGrade);
-	}
+	// printf("В структуру записаны следующие студенты:\n");
+	// for (int i = 0; i < amountOfStudents; i++) {
+	// 	printf("%s %s %d %d %d %d\n", allStudents[i].name, allStudents[i].classLetter, allStudents[i].firstGrade, allStudents[i].secondGrade, allStudents[i].thirdGrade, allStudents[i].fourthGrade);
+	// }
 
-	// Открываем файлы.
+	// Открываем файлы для записи в них данных.
 	FILE* zero = fopen("source", "w");
 	FILE* one = fopen("1", "w");
 	FILE* two = fopen("2", "w");
@@ -57,7 +62,7 @@ int main(void) {
 		printf("Ошибка открытия файла\n");
 		exit(1);
 	}
-	printf("Файл успешно открыт\n");
+	// printf("Файл успешно открыт\n");
 
 	// Распределяем студентов по файлам.
 	for (int i = 0; i < amountOfStudents; i++) {
@@ -81,7 +86,6 @@ int main(void) {
 				break;
 			}
 		}
-		printf("%d\n", getAmountOfFails(&allStudents[i]));
 		fprintf(target, "%s %s %d %d %d %d\n", allStudents[i].name, allStudents[i].classLetter, allStudents[i].firstGrade, allStudents[i].secondGrade, allStudents[i].thirdGrade, allStudents[i].fourthGrade);
 	}
 
@@ -92,6 +96,22 @@ int main(void) {
 	fclose(threeOrMore);
 	// Очищаем выделенную память.
 	free(allStudents);
+
+	// Открываем файлы заново, но уже для чтения, а не записи.
+	zero = fopen("source", "r");
+	one = fopen("1", "r");
+	two = fopen("2", "r");
+	threeOrMore = fopen("3 or more", "r");
+
+	// Выводим информацию из всех файлов.
+		printf("\033[42m%s\033[0m\n", "Содержимое исходного файла после обработки:");
+	while ((c = fgetc(zero)) != EOF) printf("%c", c);
+		printf("\033[46m%s\033[0m\n", "Содержимое файла \"1\":");
+	while ((c = fgetc(one)) != EOF) printf("%c", c);
+		printf("\033[43m%s\033[0m\n", "Содержимое файла \"2\":");
+	while ((c = fgetc(two)) != EOF) printf("%c", c);
+		printf("\033[41m%s\033[0m\n", "Содержимое файла \"3 or more\":");
+	while ((c = fgetc(threeOrMore)) != EOF) printf("%c", c);
 }
 
 // Функция получения количества долгов студента.
